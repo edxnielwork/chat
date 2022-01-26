e43b305b50de80e1dd939b4bbdf0e5f127e57662
edxnielwork <74576788+edxnielwork@users.noreply.github.com>
2022-01-26 17:42:41 -0500

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+ 
+  {
+    pathMatch: 'full',
+    path:"",
+    redirectTo:'home'
+  },
+  {
+    path:"login",
+    component: LoginComponent
+  },
+  {
+    path:"register",
+    component: RegisterComponent
+  },
+  {
+    path:"home",
+    component: HomeComponent
+  },
+  {
+    path:"**",
+    component: PageNotFoundComponent
+  },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
